14766d8923ef5e65eb5e17b0fcbda51dc7e66041
Elijah-Vakulenko <154387383+Elijah-Vakulenko@users.noreply.github.com>
2024-06-16 01:59:00 +0100

    hero/benefits/footer/work together edit
    hero - background img instaled
    hero - img jpg to png change
    work together - background img fix
    benefits - background img instaled

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import './js/header';
 import './js/hero';
 import { showDarkMode, hideDarkMode } from './js/tech-stills';
-import './js/my-projects';
+// import './js/my-projects';
 import './js/faq';
 import './js/reviews';

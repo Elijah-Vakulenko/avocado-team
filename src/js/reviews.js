6974d2d5fd9410a4be529f66abc58d9c242b9870
Evgeniy Boyko <jekaboyko0605@gmail.com>
2024-06-16 17:00:04 +0300

    fix slider

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -9,7 +9,6 @@ import axios from 'axios';
 import Swiper from 'swiper/bundle';
 import 'swiper/css';
 
-
 async function fetchAndRender() {
   const reviewsUrl = 'https://portfolio-js.b.goit.study/api/reviews';
 
@@ -21,64 +20,76 @@ async function fetchAndRender() {
       throw new Error('No reviews found');
     }
 
-      const render = reviews.map(review => `
-      <li class="swiper-slide review-item">
+    const render = reviews
+      .map(
+        review => `
+      <li class="swiper-slide">
         <p class="review-content">${review.review}</p>
+
         <div class="review-author">
           <img class="review-avatar" src="${review.avatar_url}" alt="Avatar">
           <p class="review-name">${review.author}</p>
         </div>
       </li>
-    `);
+    `
+      )
+      .join('');
 
     document.getElementById('reviews').innerHTML = render;
 
-      
-    new Swiper('.reviews-container', {
-        breakpoints: {
-          0: {
-            slidesPerView: 1,
-            spaceBetween: 0,
-          },
+    new Swiper('.swiper', {
+      direction: 'horizontal',
+      speed: 750,
+      spaceBetween: 10,
+      slidesPerView: 1,
+
+      breakpoints: {
+        768: {
+          slidesPerView: 1,
+          spaceBetween: 15,
+        },
         1280: {
-            slidesPerView: 2,
-            spaceBetween: 32,
-            slidesPerGroup: 2,
-          },
+          slidesPerView: 2,
+          spaceBetween: 32,
+        },
       },
       navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
+      },
+      simulateTouch: true,
+      touchRatio: 1,
+      grabCursor: true,
+      slideToClickedSlide: true,
+      keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+        pageUpDown: true,
+      },
+      mousewheel: {
+        sensitivity: 1,
+      },
+      on: {
+        reachEnd: function () {
+          document
+            .querySelector('.swiper-button-next')
+            .classList.add('swiper-button-disabled');
         },
-        pagination: {
-            el: 'swiper-pagination',
+        reachBeginning: function () {
+          document
+            .querySelector('.swiper-button-prev')
+            .classList.add('swiper-button-disabled');
         },
-        simulateTouch: true,
-        touchRatio: 1,
-        grabCursor: true,
-        slideToClickedSlide: true,
-        keyboard: {
-            enabled: true,
-            onlyInViewport: true,
-            pageUpDown: true,
+        fromEdge: function () {
+          document
+            .querySelector('.swiper-button-next')
+            .classList.remove('swiper-button-disabled');
+          document
+            .querySelector('.swiper-button-prev')
+            .classList.remove('swiper-button-disabled');
         },
-        mousewheel: {
-            sensitivity: 1,
-        },
-        on: {
-          reachEnd: function () {
-              document.querySelector('.swiper-button-next').classList.add('swiper-button-disabled');
-          },
-          reachBeginning: function () {
-              document.querySelector('.swiper-button-prev').classList.add('swiper-button-disabled');
-          },
-          fromEdge: function () {
-              document.querySelector('.swiper-button-next').classList.remove('swiper-button-disabled');
-              document.querySelector('.swiper-button-prev').classList.remove('swiper-button-disabled');
-          }
-        }
+      },
     });
-
   } catch (error) {
     console.error('Error fetching reviews:', error);
     const errorMessage = document.querySelector('.reviews-error-message');

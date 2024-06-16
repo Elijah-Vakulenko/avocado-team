dff18ee0d84863258e3c0f940aad56387bd90458
Evgeniy Boyko <jekaboyko0605@gmail.com>
2024-06-16 18:09:22 +0300

    result

--- src/js/functions/getRefs.js
+++ src/js/functions/getRefs.js
@@ -2,5 +2,8 @@ export default function () {
   return {
     projectsList: document.querySelector('.js-project-list'),
     loadMoreBtn: document.querySelector('[data-action="load-more"]'),
+    backToTopButton: document.querySelector(
+      'button[data-action="back-to-top"]'
+    ),
   };
 }
